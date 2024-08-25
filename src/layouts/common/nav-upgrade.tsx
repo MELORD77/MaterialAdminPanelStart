9838624eb56e1efee446d9ae32d22a01d8a671f3
Qahramonjon <qahramonmansurov98@mail.ru>
2024-08-25 12:34:47 +0500

    connetct github

---
+++ src/layouts/common/nav-upgrade.tsx
@@ -0,0 +1,38 @@
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+
+import { useMockedUser } from 'src/hooks/use-mocked-user';
+
+// ----------------------------------------------------------------------
+
+export default function NavUserInfo() {
+  const { user } = useMockedUser();
+
+  return (
+    <Stack
+      sx={{
+        px: 2,
+        py: 5,
+        textAlign: 'center',
+      }}
+    >
+      <Stack alignItems="center">
+        <Box sx={{ position: 'relative' }}>
+          <Avatar src={user?.photoURL} alt={user?.displayName} sx={{ width: 48, height: 48 }} />
+        </Box>
+
+        <Stack spacing={0.5} sx={{ mt: 1.5, mb: 2 }}>
+          <Typography variant="subtitle2" noWrap>
+            {user?.displayName}
+          </Typography>
+
+          <Typography variant="body2" noWrap sx={{ color: 'text.disabled' }}>
+            {user?.email}
+          </Typography>
+        </Stack>
+      </Stack>
+    </Stack>
+  );
+}
